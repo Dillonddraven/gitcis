a1d909e60de43962da2e358ea68e25f16c5b6c99
Dillonddraven <123018419+Dillonddraven@users.noreply.github.com>
2023-02-09 16:18:43 -0600

    Add files via upload

---
+++ mpg.js
@@ -0,0 +1,8 @@
+
+
+let floatMiles, floatGallons, floatMpg;  
+floatMiles = parseFloat(prompt("Enter miles driven"));  
+floatGallons = parseFloat(prompt("Enter gallons of gas used"));  
+floatMpg = floatMiles/floatGallons;  
+
+alert("Miles per gallon = " + floatMpg);
